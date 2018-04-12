f9b8cad61b62b068de3e2caf372fff3c759f011a
ktsn <ktsn55@gmail.com>
2018-04-12 17:30:23 +0900

    feat: add d.ts file

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import Vue, { ComponentOptions } from 'vue'
+
+export const SizeProvider: ComponentOptions<any>
+export const SizeObserver: ComponentOptions<any>
+export default function install(_Vue: typeof Vue): void
